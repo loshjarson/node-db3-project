749835dea636c45bf992168a6de701f0cd8253b6
unknown <jlarson129@outlook.com>
2021-10-19 16:58:03 -0400

    fixed issues gathered from tests

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -13,24 +13,24 @@ function find() { // EXERCISE A
 }
 
 function findById(scheme_id) { // EXERCISE B
-
+  const schemeID = Number(scheme_id)
   return db
     .select('sc.scheme_name', 'st.*')
     .from('schemes as sc')
     .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
-    .where('sc.scheme_id', scheme_id)
+    .where('sc.scheme_id', schemeID)
     .orderBy('st.step_number')
     .then((steps) => {
       if(steps[0].step_id === null) {
         const schemeSteps = {
-          'scheme_id': scheme_id,
+          'scheme_id': schemeID,
           'scheme_name': steps[0].scheme_name,
           'steps': []
         }
         return schemeSteps
       } else {
         const schemeSteps = {
-          'scheme_id': scheme_id,
+          'scheme_id': schemeID,
           'scheme_name': steps[0].scheme_name,
           'steps': steps.map(step => {
             return {
@@ -41,6 +41,7 @@ function findById(scheme_id) { // EXERCISE B
           })
         }
         return schemeSteps
+        
       }
       
     })
@@ -55,6 +56,14 @@ function findSteps(scheme_id) { // EXERCISE C
     .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
     .where('sc.scheme_id', scheme_id)
     .orderBy('st.step_id', "desc")
+    .then(steps => {
+      if (steps[0].step_number === null) {
+        return [];
+      } else {
+        return steps;
+      }
+    })
+
 }
 
 function add(scheme) { // EXERCISE D
@@ -63,6 +72,7 @@ function add(scheme) { // EXERCISE D
     .then(id => {
       return db('schemes')
       .where("scheme_id",id)
+      .first()
     })
 }
 
